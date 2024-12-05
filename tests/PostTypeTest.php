59262292e6155dd52f93e491d945a6c01d095b70
jjgrainger <github@jjgrainger.co.uk>
2024-12-05 15:18:10 +0000

    add PostType tests

--- tests/PostTypeTest.php
+++ tests/PostTypeTest.php
@@ -1,8 +1,25 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use PostTypes\PostType;
 
 class PostTypeTest extends TestCase
 {
-    //
+    public function test_post_type_returns_defaults()
+    {
+        $stub = $this->getMockForAbstractClass(PostType::class);
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $this->assertEquals('book', $stub->slug());
+        $this->assertEquals([], $stub->labels());
+        $this->assertEquals([], $stub->options());
+        $this->assertEquals([], $stub->taxonomies());
+        $this->assertEquals(['title', 'editor'], $stub->supports());
+        $this->assertEquals(null, $stub->icon());
+        $this->assertEquals([], $stub->filters());
+        $this->assertEquals(null, $stub->hooks());
+    }
 }
--- tests/Registrars/PostTypeRegistrarTest.php
+++ tests/Registrars/PostTypeRegistrarTest.php
@@ -1,8 +1,71 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use PostTypes\PostType;
+use PostTypes\Registrars\PostTypeRegistrar;
 
 class PostTypeRegistrarTest extends TestCase
 {
-    //
+    public function test_can_create_registrar()
+    {
+        $stub = $this->getMockForAbstractClass(PostType::class);
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $registrar = new PostTypeRegistrar($stub);
+
+        $this->assertInstanceOf(PostTypeRegistrar::class, $registrar);
+    }
+
+    public function test_will_modify_post_type()
+    {
+        $stub = $this->getMockForAbstractClass(PostType::class);
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $registrar = new PostTypeRegistrar($stub);
+
+        $args = [
+            'public' => false,
+        ];
+
+        $options = $registrar->modifyPostType($args, 'book');
+
+        $expected = [
+            'public'       => true,
+            'show_in_rest' => true,
+            'labels'       => [],
+            'taxonomies'   => [],
+            'supports'     => ['title', 'editor'],
+            'menu_icon'    => null,
+            'rewrite'      => [
+                'slug' => 'book',
+            ],
+        ];
+
+        $this->assertEquals($expected, $options);
+    }
+
+    public function test_will_not_modify_post_type_if_name_does_not_match()
+    {
+        $stub = $this->getMockForAbstractClass(PostType::class);
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $registrar = new PostTypeRegistrar($stub);
+
+        $args = [
+            'public' => false,
+        ];
+
+        $options = $registrar->modifyPostType($args, 'post');
+
+        $this->assertEquals($args, $options);
+    }
 }
