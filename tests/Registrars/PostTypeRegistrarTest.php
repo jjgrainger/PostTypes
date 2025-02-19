00d8241b277ffae931bbc8171dec58a5afbe9988
jjgrainger <github@jjgrainger.co.uk>
2025-02-19 14:30:48 +0000

    improve Registrar tests

--- tests/Registrars/PostTypeRegistrarTest.php
+++ tests/Registrars/PostTypeRegistrarTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use PostTypes\Column;
+use PostTypes\Columns;
 use PostTypes\PostType;
 use PostTypes\Registrars\PostTypeRegistrar;
 
@@ -67,5 +69,102 @@ class PostTypeRegistrarTest extends TestCase
         $options = $registrar->modifyPostType($args, 'post');
 
         $this->assertEquals($args, $options);
+    }
+
+    public function test_can_modify_columns()
+    {
+        $defaults = [
+            'cb' => '',
+            'title' => 'Title',
+            'author' => 'Author',
+        ];
+
+        $columns = new Columns;
+        $columns->add('date', 'Date', function() {});
+
+        $stub = $this->getMockBuilder(PostType::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $stub->expects($this->once())
+            ->method('columns')
+            ->will($this->returnValue($columns));
+
+        $registrar = new PostTypeRegistrar($stub);
+        $output = $registrar->modifyColumns($defaults);
+
+        $expected = [
+            'cb' => '',
+            'title' => 'Title',
+            'author' => 'Author',
+            'date' => 'Date',
+        ];
+
+        $this->assertEquals($expected, $output);
+    }
+
+    public function test_can_populate_column()
+    {
+        $columns = new Columns;
+
+        $stub = $this->createMock(Column::class);
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('column'));
+
+        $stub->expects($this->once())
+            ->method('populate')
+            ->will($this->returnValue(true));
+
+        $columns->column($stub);
+
+        $stub = $this->getMockBuilder(PostType::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $stub->expects($this->once())
+            ->method('columns')
+            ->will($this->returnValue($columns));
+
+        $registrar = new PostTypeRegistrar($stub);
+        $registrar->populateColumns('column', 1);
+    }
+
+    public function test_can_set_sortable_columns()
+    {
+        $columns = new Columns;
+        $columns->sortable('column', function() {});
+
+        $sortable = [
+            'title' => 'title',
+        ];
+
+        $stub = $this->getMockBuilder(PostType::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('book'));
+
+        $stub->expects($this->once())
+            ->method('columns')
+            ->will($this->returnValue($columns));
+
+        $registrar = new PostTypeRegistrar($stub);
+        $output = $registrar->setSortableColumns($sortable);
+
+        $expected = [
+            'title' => 'title',
+            'column' => 'column',
+        ];
+
+        $this->assertEquals($expected, $output);
     }
 }
--- tests/Registrars/TaxonomyRegistrarTest.php
+++ tests/Registrars/TaxonomyRegistrarTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use PostTypes\Column;
+use PostTypes\Columns;
 use PostTypes\Registrars\TaxonomyRegistrar;
 use PostTypes\Taxonomy;
 
@@ -12,11 +14,144 @@ class TaxonomyRegistrarTest extends TestCase
 
         $stub->expects($this->any())
             ->method('name')
-            ->will($this->returnValue('book'));
+            ->will($this->returnValue('genre'));
 
         $registrar = new TaxonomyRegistrar($stub);
 
         $this->assertInstanceOf(TaxonomyRegistrar::class, $registrar);
     }
 
+    public function test_can_generate_options_with_overrides()
+    {
+        $stub = $this->getMockBuilder(Taxonomy::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('genre'));
+
+        $stub->expects($this->any())
+            ->method('slug')
+            ->will($this->returnValue('genre'));
+
+
+        $stub->expects($this->once())
+            ->method('options')
+            ->will($this->returnValue([
+                'public' => false,
+            ]));
+
+
+        $registrar = new TaxonomyRegistrar($stub);
+
+        $options = $registrar->generateOptions();
+
+        $expected = [
+            'public'            => false,
+            'show_in_rest'      => true,
+            'hierarchical'      => true,
+            'show_admin_column' => true,
+            'labels'            => [],
+            'rewrite'           => [
+                'slug' => 'genre',
+            ],
+        ];
+
+        $this->assertEquals($expected, $options);
+    }
+
+    public function test_can_modify_columns()
+    {
+        $defaults = [
+            'cb' => '',
+            'name' => 'Name',
+        ];
+
+        $columns = new Columns;
+        $columns->add('popularity', 'Popularity', function() {});
+
+        $stub = $this->getMockBuilder(Taxonomy::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('genre'));
+
+        $stub->expects($this->once())
+            ->method('columns')
+            ->will($this->returnValue($columns));
+
+        $registrar = new TaxonomyRegistrar($stub);
+        $output = $registrar->modifyColumns($defaults);
+
+        $expected = [
+            'cb' => '',
+            'name' => 'Name',
+            'popularity' => 'Popularity',
+        ];
+
+        $this->assertEquals($expected, $output);
+    }
+
+    public function test_can_populate_column()
+    {
+        $columns = new Columns;
+
+        $stub = $this->createMock(Column::class);
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('column'));
+
+        $stub->expects($this->once())
+            ->method('populate')
+            ->will($this->returnValue(true));
+
+        $columns->column($stub);
+
+        $stub = $this->getMockBuilder(Taxonomy::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('genre'));
+
+        $stub->expects($this->once())
+            ->method('columns')
+            ->will($this->returnValue($columns));
+
+        $registrar = new TaxonomyRegistrar($stub);
+        $registrar->populateColumns('', 'column', 1);
+    }
+
+    public function test_can_set_sortable_columns()
+    {
+        $columns = new Columns;
+        $columns->sortable('column', function() {});
+
+        $sortable = [
+            'title' => 'title',
+        ];
+
+        $stub = $this->getMockBuilder(Taxonomy::class)
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('name')
+            ->will($this->returnValue('genre'));
+
+        $stub->expects($this->once())
+            ->method('columns')
+            ->will($this->returnValue($columns));
+
+        $registrar = new TaxonomyRegistrar($stub);
+        $output = $registrar->setSortableColumns($sortable);
+
+        $expected = [
+            'title' => 'title',
+            'column' => 'column',
+        ];
+
+        $this->assertEquals($expected, $output);
+    }
 }
